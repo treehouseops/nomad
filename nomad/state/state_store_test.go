fb84018901984848d5c07c6738ede2095466d58e
Lang Martin <lang@hashicorp.com>
2020-01-14 14:13:10 -0500

    state_store: CSIPlugin and updates, CSIVolumeDenormalize

--- nomad/state/state_store_test.go
+++ nomad/state/state_store_test.go
@@ -2920,6 +2920,130 @@ func TestStateStore_CSIVolume(t *testing.T) {
 	require.True(t, vs[0].CanReadOnly())
 }
 
+// TestStateStore_CSIPluginJobs creates plugin jobs and tests that they create a CSIPlugin
+func TestStateStore_CSIPluginJobs(t *testing.T) {
+	index := uint64(999)
+	state := testStateStore(t)
+	testStateStore_CSIPluginJobs(t, index, state)
+}
+
+func testStateStore_CSIPluginJobs(t *testing.T, index uint64, state *StateStore) (uint64, *StateStore) {
+	j0 := mock.Job()
+	j0.TaskGroups[0].Tasks[0].CSIPluginConfig = &structs.TaskCSIPluginConfig{
+		ID:     "foo",
+		Driver: "adam",
+		Type:   structs.CSIPluginTypeController,
+	}
+
+	j1 := mock.Job()
+	j1.Type = structs.JobTypeSystem
+	j1.TaskGroups[0].Tasks[0].CSIPluginConfig = &structs.TaskCSIPluginConfig{
+		ID:     "foo",
+		Driver: "adam",
+		Type:   structs.CSIPluginTypeNode,
+	}
+
+	index++
+	err := state.UpsertJob(index, j0)
+	require.NoError(t, err)
+
+	index++
+	err = state.UpsertJob(index, j1)
+	require.NoError(t, err)
+
+	// Get the plugin back out by id
+	ws := memdb.NewWatchSet()
+	plug, err := state.CSIPluginByID(ws, "foo")
+	require.NoError(t, err)
+
+	require.Equal(t, "foo", plug.ID)
+	require.Equal(t, "adam", plug.Driver)
+
+	jids := map[string]struct{}{j0.ID: struct{}{}, j1.ID: struct{}{}}
+	for jid := range plug.Jobs[structs.DefaultNamespace] {
+		delete(jids, jid)
+	}
+	require.Equal(t, 0, len(jids))
+
+	return index, state
+}
+
+// TestStateStore_CSIPluginNodes uses the state from jobs, and uses node fingerprinting to update health
+func TestStateStore_CSIPluginNodes(t *testing.T) {
+	index := uint64(999)
+	state := testStateStore(t)
+	index, state = testStateStore_CSIPluginJobs(t, index, state)
+	testStateStore_CSIPluginNodes(t, index, state)
+}
+
+func testStateStore_CSIPluginNodes(t *testing.T, index uint64, state *StateStore) (uint64, *StateStore) {
+	// Create Nodes fingerprinting the plugins
+	ns := []*structs.Node{mock.Node(), mock.Node(), mock.Node()}
+
+	for _, n := range ns {
+		index++
+		err := state.UpsertNode(index, n)
+		require.NoError(t, err)
+	}
+
+	// Fingerprint a running controller plugin
+	n0 := ns[0].Copy()
+	n0.CSIControllerPlugins = map[string]*structs.CSIInfo{
+		"foo": {
+			PluginID:                 "foo",
+			Healthy:                  true,
+			UpdateTime:               time.Now(),
+			RequiresControllerPlugin: true,
+			RequiresTopologies:       false,
+			ControllerInfo: &structs.CSIControllerInfo{
+				SupportsReadOnlyAttach: true,
+				SupportsListVolumes:    true,
+			},
+		},
+	}
+
+	index++
+	err := state.UpsertNode(index, n0)
+	require.NoError(t, err)
+
+	// Fingerprint two running node plugins
+	for _, n := range ns[1:] {
+		n = n.Copy()
+		n.CSINodePlugins = map[string]*structs.CSIInfo{
+			"foo": {
+				PluginID:                 "foo",
+				Healthy:                  true,
+				UpdateTime:               time.Now(),
+				RequiresControllerPlugin: true,
+				RequiresTopologies:       false,
+				NodeInfo:                 &structs.CSINodeInfo{},
+			},
+		}
+
+		index++
+		err = state.UpsertNode(index, n)
+		require.NoError(t, err)
+	}
+
+	ws := memdb.NewWatchSet()
+	plug, err := state.CSIPluginByID(ws, "foo")
+	require.NoError(t, err)
+
+	require.Equal(t, "foo", plug.ID)
+	require.Equal(t, 1, plug.ControllerHealthy)
+	require.Equal(t, 2, plug.NodeHealthy)
+
+	return index, state
+}
+
+// TestStateStore_CSIPluginBackwards gets the node state first, and the job state second
+func TestStateStore_CSIPluginBackwards(t *testing.T) {
+	index := uint64(999)
+	state := testStateStore(t)
+	index, state = testStateStore_CSIPluginNodes(t, index, state)
+	testStateStore_CSIPluginJobs(t, index, state)
+}
+
 func TestStateStore_Indexes(t *testing.T) {
 	t.Parallel()
 
